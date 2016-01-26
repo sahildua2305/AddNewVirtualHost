8dab82657c5bf28720facf467832c35eff0e533c
Sahil Dua <sahildua2305@gmail.com>
2016-01-27 05:26:38 +0530

    Added detailed comments

--- add-site-localhost.sh
+++ add-site-localhost.sh
@@ -2,9 +2,10 @@
 # @Author: sahildua2305
 # @Date:   2016-01-27 03:37:25
 # @Last Modified by:   sahildua2305
-# @Last Modified time: 2016-01-27 05:14:21
+# @Last Modified time: 2016-01-27 05:26:17
 
 
+# Create configuration file for the new virtual host in `sites-available` folder
 sudo cat <<EOF >/etc/apache2/sites-available/$1.conf
 <VirtualHost $2:80>
 DocumentRoot "/var/www/html/$1/"
@@ -19,12 +20,14 @@ AllowOverride All
 </VirtualHost>
 EOF
 
-# sudo cp /etc/apache2/sites-available/$1.conf  /etc/apache2/sites-enabled/$1.conf
-
+# Enable the newly added configuration file for the new virtual host
 sudo a2ensite $1.conf
 
+# Reload apache2 service
 sudo service apache2 reload
 
+# Add the new virtual host URL to `/etc/hosts`
 sudo echo "127.0.0.1 	$2" >> /etc/hosts
 
+# Restart apache2 service
 sudo service apache2 restart
